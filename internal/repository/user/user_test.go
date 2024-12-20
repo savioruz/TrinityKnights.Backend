bf16ae7c4898c08e0442fe0a82dc169e8f9a9029
jakues <jakueenak@gmail.com>
2024-12-20 21:19:57 +0700

    test(user): update user update test with last login

--- internal/repository/user/user_test.go
+++ internal/repository/user/user_test.go
@@ -97,6 +97,7 @@ func TestRepositoryImpl_Update(t *testing.T) {
 		Name:               "Test User",
 		Role:               "admin",
 		Status:             true,
+		LastLogin:          nil,
 		VerifyEmailToken:   "",
 		IsVerified:         false,
 		ResetPasswordToken: "",
@@ -104,13 +105,14 @@ func TestRepositoryImpl_Update(t *testing.T) {
 
 	// Mock the query for Update
 	mock.ExpectBegin()
-	mock.ExpectExec(regexp.QuoteMeta("UPDATE `users` SET `email`=?,`password`=?,`name`=?,`role`=?,`status`=?,`verify_email_token`=?,`reset_password_token`=?,`is_verified`=?,`created_at`=?,`updated_at`=?,`deleted_at`=? WHERE `users`.`deleted_at` IS NULL AND `id` = ?")).
+	mock.ExpectExec(regexp.QuoteMeta("UPDATE `users` SET `email`=?,`password`=?,`name`=?,`role`=?,`status`=?,`last_login`=?,`verify_email_token`=?,`reset_password_token`=?,`is_verified`=?,`created_at`=?,`updated_at`=?,`deleted_at`=? WHERE `users`.`deleted_at` IS NULL AND `id` = ?")).
 		WithArgs(
 			expectedUser.Email,
 			expectedUser.Password,
 			expectedUser.Name,
 			expectedUser.Role,
 			expectedUser.Status,
+			expectedUser.LastLogin,
 			expectedUser.VerifyEmailToken,
 			expectedUser.ResetPasswordToken,
 			expectedUser.IsVerified,
